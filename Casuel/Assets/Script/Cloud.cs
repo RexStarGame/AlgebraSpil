51d18dc6a57d71c8c36f3061e075284dcc2a51c2
lilmonix3UwU <Tbnielsen2005@gmail.com>
2025-02-05 21:53:26 +0100

    clouds for everyone

--- Casuel/Assets/Script/Cloud.cs
+++ Casuel/Assets/Script/Cloud.cs
@@ -4,11 +4,12 @@ public class Cloud : MonoBehaviour
 {
     public float speed;
     public int xDirection;
+    public int bounds;
 
     void Update()
     {
         transform.Translate(xDirection * speed * Time.deltaTime, 0, 0);
-        if (transform.position.x < -15 || transform.position.x > 15)
+        if (transform.position.x < -bounds || transform.position.x > bounds)
         {
             Destroy(gameObject);
         }
--- Casuel/Assets/Script/CloudManager.cs
+++ Casuel/Assets/Script/CloudManager.cs
@@ -5,6 +5,8 @@ public class CloudManager: MonoBehaviour
     [SerializeField] GameObject[] _clouds;
     [SerializeField] float _cloudSpeed;
     [SerializeField] float _cloudDelay;
+    [SerializeField] int _xSpawningRange;
+    [SerializeField] int _ySpawningRange;
     float timer;
 
 
@@ -14,26 +16,26 @@ public class CloudManager: MonoBehaviour
 
         if (timer < 0 )
         {
-            Debug.Log("pp");
             int tempX;
             int tempRand = Random.Range(1, 3);
             int tempXStart;
             if ( tempRand == 1 )
             {
                 tempX = -1;
-                tempXStart = 5;
+                tempXStart = _xSpawningRange;
             }
             else
             {
                 tempX = 1;
-                tempXStart = -5;
+                tempXStart = -_xSpawningRange;
             }
 
 
-            GameObject tempCloud = Instantiate(_clouds[Random.Range(0, _clouds.Length)],new Vector3(tempXStart, Random.Range(-5, 5), 0), transform.rotation);
+            GameObject tempCloud = Instantiate(_clouds[Random.Range(0, _clouds.Length)],new Vector3(tempXStart, Random.Range(-_ySpawningRange, _ySpawningRange), 0), transform.rotation);
 
             tempCloud.GetComponent<Cloud>().speed = _cloudSpeed;
             tempCloud.GetComponent<Cloud>().xDirection = tempX;
+            tempCloud.GetComponent<Cloud>().bounds = _xSpawningRange + 5;
 
             timer = _cloudDelay;
 
